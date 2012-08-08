19e86ef17f765bbeafa49e5907fb6b99627740b2
Kaijen Hsiao <hsiao@willowgarage.com>
2012-08-07 19:52:58 -0700

    switched torso_up.py to use the torso_controller/position_joint_action so it stops chugging continuously

--- pr2_position_scripts/scripts/torso_up.py
+++ pr2_position_scripts/scripts/torso_up.py
@@ -4,21 +4,18 @@ roslib.load_manifest('pr2_position_scripts')
 
 import rospy
 import actionlib
-from trajectory_msgs.msg import *
+from pr2_controllers_msgs.msg import SingleJointPositionAction, SingleJointPosi\
+tionGoal
 
-
-
-torso_pub= rospy.Publisher('torso_controller/command', JointTrajectory)
 rospy.init_node('torso_up')
-
-while not rospy.is_shutdown():
-    traj = JointTrajectory()
-    traj.header.stamp = rospy.get_rostime()
-    traj.joint_names.append("torso_lift_joint");
-    traj.points.append(JointTrajectoryPoint())
-    traj.points[0].positions.append(0.31)
-    traj.points[0].velocities.append(0.1)
-    traj.points[0].time_from_start = rospy.Duration(0.25)
-    torso_pub.publish(traj)
-    rospy.sleep(rospy.Duration.from_sec(0.2))
+torso_action_client = actionlib.SimpleActionClient('torso_controller/position_j\
+oint_action', SingleJointPositionAction);
+rospy.loginfo("torso_up: waiting for torso action server")
+torso_action_client.wait_for_server()
+rospy.loginfo("torso_up: torso action server found")
+goal = SingleJointPositionGoal()
+goal.position = 0.295
+rospy.loginfo("torso_up: sending command to lift torso")
+torso_action_client.send_goal(goal)
+torso_action_client.wait_for_result(rospy.Duration(30))
 
